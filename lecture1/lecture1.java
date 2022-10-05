f962fc1b86210e7f22732dfc3301490bb59d35a5
Milopadma <48256865+Milopadma@users.noreply.github.com>
2022-10-05 14:33:08 +0800

    code comments

--- lecture1/lecture1.java
+++ lecture1/lecture1.java
@@ -1,17 +1,17 @@
 
 class gui {
     public static void main(String[] args) {
-        // initialize components
-        javax.swing.JFrame frame = new javax.swing.JFrame("Hello World");
-        javax.swing.JLabel label = new javax.swing.JLabel("Hello World");
-        javax.swing.JButton button = new javax.swing.JButton("Click me");
-        javax.swing.JPanel panel = new javax.swing.JPanel();
+        // initialize gui components
+        javax.swing.JFrame frame = new javax.swing.JFrame("Hello World"); // the window box
+        javax.swing.JLabel label = new javax.swing.JLabel("Hello World"); // the text label
+        javax.swing.JButton button = new javax.swing.JButton("Click me"); // the button
+        javax.swing.JPanel panel = new javax.swing.JPanel(); // the panel to hold the components (inside frame)
+        // add panel to frame
+        frame.add(panel);
         // add components to panel
         panel.add(label);
         panel.add(button);
-        // add panel to frame
-        frame.add(panel);
-        // set frame properties
+        // set frame properties //this apparently needs to be last
         frame.setDefaultCloseOperation(javax.swing.JFrame.EXIT_ON_CLOSE);
         frame.setSize(300, 300);
         frame.setVisible(true);
