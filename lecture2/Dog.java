e3650fe7f972da9f1b37b663cbbc1e5311e87c09
Milopadma <Milopadma@yahoo.com>
2022-10-11 13:27:07 +0800

    Dog class improvements

--- lecture2/Dog.java
+++ lecture2/Dog.java
@@ -30,9 +30,30 @@ public class Dog {
         this.age = age;
     }
 
-    // toString
-    public String toString() {
-        return "Dog [name = " + name + ", age = " + age + "]";
+    // other methods
+    // to person years methods
+    // since 1 dog year is equivalent to 7 human years
+    public int toPersonYears() {
+        return age * 7;
     }
 
+    // toString
+    public String toString() {
+        return "Dog [name = " + name + ", age = " + age + " with human years of " + toPersonYears() + "]";
+    }
+
+}
+
+// DogTest class (Kennel?)
+
+class Kennel {
+    public static void main(String[] args) {
+        Dog dog1 = new Dog("Roger", 2);
+        Dog dog2 = new Dog("Rex", 50);
+        Dog dog3 = new Dog("Rover", 7);
+
+        System.out.println(dog1);
+        System.out.println(dog2);
+        System.out.println(dog3);
+    }
 }
