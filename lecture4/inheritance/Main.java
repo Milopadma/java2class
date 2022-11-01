5cde323994b87565914fbb797eb5fb3fa3d01587
Milopadma <48256865+Milopadma@users.noreply.github.com>
2022-11-01 19:02:20 +0800

    more

--- lecture4/inheritance/Main.java
+++ lecture4/inheritance/Main.java
@@ -4,16 +4,21 @@ public class Main {
     public static void main(String[] args) {
         // BangunDatar object
         BangunDatar bangunDatar = new BangunDatar();
+
         // persegi object
-        Persegi persegi = new Persegi(10);
+        Persegi persegi = new Persegi(10); // sisi 
 
         // lingkaran object
-        Lingkaran lingkaran = new Lingkaran(10);
+        Lingkaran lingkaran = new Lingkaran(10); //radius
 
         // segitiga object
-        Segitiga segitiga = new Segitiga(10, 10);
+        Segitiga segitiga = new Segitiga(10, 10); //alas, tinggi
 
-
+        bangunDatar.luas();
+        persegi.luas();
+        segitiga.luas();
+        lingkaran.luas();
+        lingkaran.keliling();
 
 
     }
