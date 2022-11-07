45923e8d67dac4435431863af6540ebb49c145ae
Milopadma <48256865+Milopadma@users.noreply.github.com>
2022-11-07 22:59:02 +0800

    new samsung class

--- lecture5/interface/Samsung.java
+++ lecture5/interface/Samsung.java
@@ -1,3 +1,28 @@
 public class Samsung {
-    
+    // class variables
+    private boolean isOn;
+    private boolean isPowerOn;
+
+    // constructor
+    public Samsung() {
+        this.isOn = false;
+        this.isPowerOn = false;
+    }
+
+    // class methods
+    public void powerOn() {
+        isPowerOn = true;
+        System.out.println("Power on");
+        System.out.println("Samsung is on");
+    }
+
+    public void powerOff() {
+        isPowerOn = false;
+    }
+
+    public void volumeUp() {
+        if (isPowerOn) {
+            System.out.println("Volume up");
+        }
+    }
 }
