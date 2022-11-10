d970496ad94cce19a87728941a780e33c77ea183
Milopadma <48256865+Milopadma@users.noreply.github.com>
2022-11-10 16:02:31 +0800

    type-safety

--- assignment1/SchoolHELP.java
+++ assignment1/SchoolHELP.java
@@ -51,8 +51,11 @@ public class SchoolHELP {
     public boolean isUserAdmin(String adminUsername, String adminPassword){
         // find this user in the users arraylist
         User user = users.stream().filter(u -> u.getUsername().equals(adminUsername) && u.getPassword().equals(adminPassword)).findAny().orElse(null);
+        if (user == null) {
+            return false;
+        }
         // check if this user instance is a SchoolAdmin or not
-        if(user.isSchoolAdmin() == true){
+        if(user.isSchoolAdmin()){
             return true;
         }else{
             return false;
@@ -62,7 +65,9 @@ public class SchoolHELP {
     public boolean isUserVolunteer(String volunteerUsername, String volunteerPassword){
         // find this user in the users arraylist
         User user = users.stream().filter(u -> u.getUsername().equals(volunteerUsername) && u.getPassword().equals(volunteerPassword)).findAny().orElse(null);
-        // check if this user instance is a Volunteer or not
+        if (user == null) {
+            return false;
+        }       // check if this user instance is a Volunteer or not
         if(user.isVolunteer() == true){
             return true;
         }else{
