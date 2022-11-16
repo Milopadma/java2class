3666c6076d446d6cabc473124a431265a8474455
Milopadma <48256865+Milopadma@users.noreply.github.com>
2022-11-16 11:02:14 +0800

    changed all to use stream and lambda exps

--- test1/Student.java
+++ test1/Student.java
@@ -1,3 +1,8 @@
+
+// imports
+import java.io.*;
+import java.util.*;
+
 public class Student {
     // class headers
     private String name;
@@ -14,67 +19,39 @@ public class Student {
 
     // addSubject that adds the passed in Subject to the collection of Subjects
     public void addSubject(Subject subject) {
-        this.subjects.add(subject);
+        subjects.add(subject);
     }
 
     // allSubject method that returns a string containing all the enrolled subjects,
     // one per line
     public String allSubjects() {
-        String allSubjects = "";
-        for (Subject subject : this.subjects) {
-            allSubjects += subject.toString() + "\n";
-        }
-        return allSubjects;
+        return this.subjects.stream().map(Subject::toString).collect(Collectors.joining("\n"));
     }
 
     // numPaidSubjects method that returns a number of FullFeeSubjects
     public int numPaidSubjects() {
-        int count = 0;
-        for (Subject subject : this.subjects) {
-            if (subject instanceof FullFeeSubject) { // if the subject is a FullFeeSubject
-                count++;
-            }
-        }
-        return count;
+        return (int) this.subjects.stream().filter(subject -> subject instanceof FullFeeSubject)
+                .count();
     }
 
     // totalCostPaidSubjects method that returns the total number of fees paid for
     // FullFeeSubjects
     public double totalCostPaidSubjects() {
-        double totalCost = 0;
-        for (Subject subject : this.subjects) {
-            if (subject instanceof FullFeeSubject) { // if the subject is a FullFeeSubject
-                totalCost += ((FullFeeSubject) subject).getCost(); // call subjects' getCost
-            }
-        }
-        return totalCost;
+        return this.subjects.stream().filter(subject -> subject instanceof FullFeeSubject)
+                .mapToDouble(subject -> ((FullFeeSubject) subject).getCost()).sum();
     }
 
     // subjectSearch method that accepts a representing name of a subject and
     // returns the subject that matches it
     public Subject subjectSearch(String name) {
-        for (Subject subject : this.subjects) {
-            // return the subject if it is found
-            if (subject.getName().equals(name)) {
-                return subject;
-            }
-        }
-        // if its not found, return null
-        return null;
+        return this.subjects.stream().filter(subject -> subject.getName().equals(name)).findFirst()
+                .orElse(null);
     }
 
     // subjectCodeSearch method that takes the first three characters of a subject
     // code and returns a list that contains all the subjects that matches
     public ArrayList<Subject> subjectCodeSearch(String code) {
-        // init the collection that will be returned
-        ArrayList<Subject> subjects = new ArrayList<Subject>();
-        for (Subject subject : this.subjects) {
-            // if the subject code starts with the code passed in, add it to the list
-            if (subject.getCode().startsWith(code)) {
-                subjects.add(subject);
-            }
-        }
-        // return the collection of subjects
-        return subjects;
+        return this.subjects.stream().filter(subject -> subject.getCode().startsWith(code))
+                .collect(Collectors.toList());
     }
 }
