6f39d2ce22a97387eb6596871b3713222e6511ad
Milopadma <48256865+Milopadma@users.noreply.github.com>
2022-11-16 10:56:13 +0800

    new methods

--- test1/Student.java
+++ test1/Student.java
@@ -49,4 +49,32 @@ public class Student {
         }
         return totalCost;
     }
+
+    // subjectSearch method that accepts a representing name of a subject and
+    // returns the subject that matches it
+    public Subject subjectSearch(String name) {
+        for (Subject subject : this.subjects) {
+            // return the subject if it is found
+            if (subject.getName().equals(name)) {
+                return subject;
+            }
+        }
+        // if its not found, return null
+        return null;
+    }
+
+    // subjectCodeSearch method that takes the first three characters of a subject
+    // code and returns a list that contains all the subjects that matches
+    public ArrayList<Subject> subjectCodeSearch(String code) {
+        // init the collection that will be returned
+        ArrayList<Subject> subjects = new ArrayList<Subject>();
+        for (Subject subject : this.subjects) {
+            // if the subject code starts with the code passed in, add it to the list
+            if (subject.getCode().startsWith(code)) {
+                subjects.add(subject);
+            }
+        }
+        // return the collection of subjects
+        return subjects;
+    }
 }
