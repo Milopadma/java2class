57dc7f3aaabe25081e4dc1085257bc17b4a951a5
mylo <milopadma@yahoo.com>
2022-12-21 13:00:48 +0800

    end of lecture

--- lecture8/TestEventFrame.java
+++ lecture8/TestEventFrame.java
@@ -9,25 +9,30 @@ public class TestEventFrame extends JFrame implements ActionListener {
     private JLabel lbl;
     private int count = 0;
 
+    // main method
     public static void main(String[] args) {
         TestEventFrame test = new TestEventFrame();
         test.setVisible(true);
     }
 
+    // constructor
     public TestEventFrame() {
         super("Test Event Frame");
         setSize(300, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLayout(new FlowLayout());
 
+        // create a button
         btn = new JButton("Click Me");
         btn.addActionListener(this);
         add(btn);
 
+        // create a label
         lbl = new JLabel("Clicks: 0");
         add(lbl);
     }
 
+    // action listener
     public void actionPerformed(ActionEvent e) {
         count++;
         lbl.setText("Clicks: " + count);
