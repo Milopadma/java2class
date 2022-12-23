3bdb952161d680bca97738dcf1720c7a49bde974
mylo <milopadma@yahoo.com>
2022-12-23 22:38:04 +0800

    LoginScreen abstract class

---
+++ assignment2/LoginScreen.java
@@ -0,0 +1,35 @@
+// this class handles everything related to the login screen, the first class to be called when the program is run
+// and it also handles both AdminLogin and VolunteerLogin classes
+public abstract class LoginScreen {
+    // class fields
+    private String username;
+    private String password;
+
+    // constructor
+    public LoginScreen(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    // getters and setters
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    // abstract methods
+    public abstract void login();
+
+    public abstract void logout();
+
+    public abstract void register();
+
+    public abstract void forgotPassword();
+
+    public abstract void changePassword();
+
+    public abstract void changeUsername();
+}
