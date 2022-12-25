12a258d79afdc8b8e3dd40705d2a1d9c26b92d8d
mylo <milopadma@yahoo.com>
2022-12-25 22:04:57 +0800

    oh my god its finally centered

--- assignment2/LoginViewPanel.java
+++ assignment2/LoginViewPanel.java
@@ -6,33 +6,37 @@ import javax.swing.*;
 
 public class LoginViewPanel extends JPanel {
     // a generic 2 choice button for the initial login screen
-    private JPanel new_panel = new JPanel();
     // class fields
     private JButton ExitButton = new JButton("Exit");
 
     // constructor
     public LoginViewPanel(JButton AdminButton, JButton VolunteerButton, String title) {
-
-        new_panel.setBackground(Color.RED);
-        // different labels and button functionalities
         JLabel title_label = new JLabel(title);
+
         AdminButton.setText("Admin");
         VolunteerButton.setText("Volunteer");
 
         JPanel center_panel = new JPanel();
+        // center panel layout, items are stacked vertically
         center_panel.setLayout(new BoxLayout(center_panel, BoxLayout.Y_AXIS));
         center_panel.add(title_label);
         center_panel.add(AdminButton);
         center_panel.add(VolunteerButton);
         center_panel.add(ExitButton);
 
-        new_panel.setLayout(new BorderLayout(1, 1));
+        // parent panel layout, items are stacked horizontally
+        setLayout(new BoxLayout(this, 2));
 
-        new_panel.add(center_panel, BorderLayout.CENTER);
-        // set the layout of the main panel to center thisPanel
-        add(new_panel, BorderLayout.CENTER);
+        // to center the center panel
+        setLayout(new GridBagLayout());
+        // to add the center panel to the parent panel
+        add(center_panel, new GridBagConstraints());
 
         // event handlers
         ExitButton.addActionListener(e -> System.exit(0));
     }
 }
+
+// notes
+// adding the centerpanel to another parent panel seems to just cover the empty
+// parent panel
