540ddef580e02d51b4e0a3aa9d0df79c07871f2b
mylo <milopadma@yahoo.com>
2023-01-16 23:02:31 +0800

    tomorrow

---
+++ exam-practice/aggregationInheritance.java
@@ -0,0 +1,9 @@
+// this class shows the example snippet codes for 
+// aggregation and inheritance
+public class aggregationInheritance {
+    
+}
+
+class inheritance extends aggregationInheritance {
+    // this class inherits from aggregationInheritance
+}
